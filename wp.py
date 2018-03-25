a4c828cc033234b4bc2d0a1ec0fe4b54c92fb3ef
sailesh1999 <sailesh292@gmail.com>
2018-03-25 17:38:53 +0530

    Added comments

--- wp.py
+++ wp.py
@@ -3,61 +3,50 @@ from time import sleep
 import sys
 import webbrowser
 
+path1=' '  #Paste the path of "searchbar.png" image here         
+path2=' '    #Paste the path of "msgbox.png" image here
 
-	
-name=input('Enter name: ')
-no=int(input('Enter no of times message should be sent: '))
-msg=input('Enter message that is to be sent: ')
 
-controller=webbrowser.get('google-chrome')
-#m=57
-#n=538
+name = input('Enter name: ')
+no = int(input('Enter no of times message should be sent: '))
+msg = input('Enter message that is to be sent: ')
+
+controller = webbrowser.get('google-chrome')
+
 
 controller.open('https://web.whatsapp.com/')
-coOr=None
-x=1
-while coOr==None:
-	sleep(5)
-	coOr=locateOnScreen('/home/sailesh/Desktop/searchbar.png')
-	'''print('iteration',x)
-	x+=1
-	'''
+coOr = None
+x = 1
 
-'''
-coOr=locateOnScreen('/home/sailesh/Desktop/searchbar.png')
-print(coOr)
-'''
-x1,y1=center(coOr)
-#x2,y2=size()
-moveTo(x1,y1)
+while coOr == None:
+    sleep(5)
+    coOr = locateOnScreen(path1)
+
+
+
+x1, y1 = center(coOr)
+
+moveTo(x1, y1)
 click()
-typewrite(name,interval=0.2)
+typewrite(name, interval=0.2)
 sleep(2)
 press('enter')
 
-c=confirm(text="Did the program selected right person? Note: Once started, the program can't be terminated until task is completed", title='Continue?', buttons=['Yes', 'Cancel'])
+c = confirm(
+    text="Did the program selected right person? Note: Once started, the program can't be terminated until task is completed",
+    title='Continue?', buttons=['Yes', 'Cancel'])
 
 
-#c=input('Continue (y/n)')
-if c=='Cancel':
-	sys.exit()
+if c == 'Cancel':
+    sys.exit()
 
-'''
-x=x1
-y=((m*y2)+(n*y1))/(m+n)
-print(y)
 
-moveTo(x,y)
-click()
-sleep(5)
-'''
-x3,y3=locateCenterOnScreen('/home/sailesh/Desktop/msgbox.png')
-moveTo(x3,y3)
+x3, y3 = locateCenterOnScreen(path2)
+moveTo(x3, y3)
 click()
 sleep(2)
 for i in range(no):
     typewrite(msg)
-    press('enter')	
+    press('enter')
 
-print('program completed')
-
+print('Program completed')
